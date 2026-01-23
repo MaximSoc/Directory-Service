9a68ef4aaae78743028f3645af4f08fc1eaab181
socihovskiy <mr.socihovskiy@mail.ru>
2026-01-23 11:50:58 +0700

    Добавлен функционал для обработки ошибок

--- client/src/shared/api/axios-instance.ts
+++ client/src/shared/api/axios-instance.ts
@@ -1,3 +1,27 @@
 import axios from "axios";
+import { Envelope } from "./envelope";
+import { EnvelopeError } from "./errors";
 
 export const apiClient = axios.create({ baseURL: "http://localhost:8080/api" });
+
+apiClient.interceptors.response.use(
+  (response) => {
+    const data = response.data as Envelope;
+
+    if (data.isError && data.error) {
+    }
+
+    return response;
+  },
+  (error) => {
+    if (axios.isAxiosError(error) && error.response?.data) {
+      const envelope = error.response.data as Envelope;
+
+      if (envelope?.isError && envelope.error) {
+        throw new EnvelopeError(envelope.error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
